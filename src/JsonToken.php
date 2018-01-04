8445d78ec048a34a498e098ad9ad6749227ec685
Paragon Initiative Enterprises <security@paragonie.com>
2018-01-04 16:16:11 -0500

    Remove unused type import.

--- src/JsonToken.php
+++ src/JsonToken.php
@@ -11,7 +11,6 @@ use ParagonIE\PAST\Exception\{
     PastException
 };
 use ParagonIE\PAST\Keys\{
-    AsymmetricPublicKey,
     AsymmetricSecretKey,
     SymmetricAuthenticationKey,
     SymmetricEncryptionKey
