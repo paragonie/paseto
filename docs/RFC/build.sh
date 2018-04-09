f244ea9773cd229474e7b65d8c2cdf9b928450c0
Paragon Initiative Enterprises <security@paragonie.com>
2018-04-09 17:03:09 -0400

    Rename file to be consistent with the RFC Editors' expectations.

--- docs/RFC/build.sh
+++ docs/RFC/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mmark -xml2 -page paseto.md > paseto.xml
-xml2rfc --text paseto.xml
-rm paseto.xml
+mmark -xml2 -page paseto.md > draft-paragonie-paseto-rfc-00.xml
+xml2rfc --text draft-paragonie-paseto-rfc-00.xml
+rm draft-paragonie-paseto-rfc-00.xml
 
