9874452d9ef7e4cb218c37eab3f6afec5fdd94f2
Paragon Initiative Enterprises <security@paragonie.com>
2018-04-19 09:20:39 -0400

    Future builds will target draft01

--- docs/RFC/build.sh
+++ docs/RFC/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mmark -xml2 -page paseto.md > draft-paragonie-paseto-rfc-00.xml
-xml2rfc --text draft-paragonie-paseto-rfc-00.xml
-rm draft-paragonie-paseto-rfc-00.xml
+mmark -xml2 -page paseto.md > draft-paragonie-paseto-rfc-01.xml
+xml2rfc --text draft-paragonie-paseto-rfc-01.xml
+rm draft-paragonie-paseto-rfc-01.xml
 
