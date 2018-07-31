b2c5c1c60b33cc21711f812efd2957d56df2cc5e
Martijn Pieters <mj@zopatista.com>
2018-07-31 15:51:42 +0200

    Consistently number tests
    There were two S-6 tests in this file.

--- tests/Version2VectorTest.php
+++ tests/Version2VectorTest.php
@@ -293,7 +293,7 @@ class Version2VectorTest extends TestCase
         $this->assertSame(
             'v2.public.eyJkYXRhIjoidGhpcyBpcyBhIHNpZ25lZCBtZXNzYWdlIiwiZXhwaXJlcyI6IjIwMTktMDEtMDFUMDA6MDA6MDArMDA6MDAifcMYjoUaEYXAtzTDwlcOlxdcZWIZp8qZga3jFS8JwdEjEvurZhs6AmTU3bRW5pB9fOQwm43rzmibZXcAkQ4AzQs.UGFyYWdvbiBJbml0aWF0aXZlIEVudGVycHJpc2Vz',
             Version2::sign($message, $this->privateKey, $footer),
-            'Test Vector S-6'
+            'Test Vector S-7'
         );
 
         $message = \json_encode(['data' => 'this is a signed message', 'exp' => '2019-01-01T00:00:00+00:00']);
@@ -301,7 +301,7 @@ class Version2VectorTest extends TestCase
         $this->assertSame(
             'v2.public.eyJkYXRhIjoidGhpcyBpcyBhIHNpZ25lZCBtZXNzYWdlIiwiZXhwIjoiMjAxOS0wMS0wMVQwMDowMDowMCswMDowMCJ9flsZsx_gYCR0N_Ec2QxJFFpvQAs7h9HtKwbVK2n1MJ3Rz-hwe8KUqjnd8FAnIJZ601tp7lGkguU63oGbomhoBw.eyJraWQiOiJ6VmhNaVBCUDlmUmYyc25FY1Q3Z0ZUaW9lQTlDT2NOeTlEZmdMMVc2MGhhTiJ9',
             Version2::sign($message, $this->privateKey, $footer),
-            'Test Vector S-7'
+            'Test Vector S-8'
         );
     }
 }
