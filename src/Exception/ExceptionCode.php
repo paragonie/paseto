d969faba3b807a0e57d43264b65167be9996ebaa
Paragon Initiative Enterprises <security@paragonie.com>
2022-06-02 17:21:06 -0400

    Use new match operator

--- src/Exception/ExceptionCode.php
+++ src/Exception/ExceptionCode.php
@@ -46,58 +46,57 @@ abstract class ExceptionCode
      */
     public static function explainErrorCode(int $code): string
     {
-        switch ($code) {
-            case self::BAD_VERSION:
-                return "PASETO is very pedantic about versions. " .
-                    "If you're getting this message, you were insufficiently pedantic in how you called this feature.";
-            case self::CLAIM_JSON_TOO_LONG:
-                return "The length of the JSON-encoded claim sis longer than the Parser is configured to accept.";
-            case self::CLAIM_JSON_TOO_MANY_KEYs:
-                return "The number of object keys in the JSON-encoded claims exceeds the configured limit. " .
-                    "This may be a Hash-DoS attempt, but it's most likely a misconfiguration.";
-            case self::CLAIM_JSON_TOO_DEEP:
-                return "This JSON-encoded claims has too much recursive depth.";
-            case self::FOOTER_JSON_ERROR:
-                return "A JSON error occurred when JSON-serializing or deserializing the footer.";
-            case self::FOOTER_MISMATCH_EXPECTED:
-                return "We were expecting a different footer than the payload contained.";
-            case self::HKDF_BAD_LENGTH:
-                return "At some point, HKDF was called incorrectly. " .
-                    "This shouldn't ever happen in practice. Make sure your copy of this library imported correctly.";
-            case self::IMPLICIT_ASSERTION_JSON_ERROR:
-                return "A JSON error occurred when JSON-serializing or deserializing the Implicit Assertions.";
-            case self::IMPLICIT_ASSERTION_NOT_SUPPORTED:
-                return "Implicit Assertions are only a part of Version 3 and Version 4. " .
-                    "This feature is explicitly not permitted in older versions to ensure backwards compatibility.";
-            case self::INVALID_BASE64URL:
-                return "An error occurred when attempting to decode what we expected to be a base64url-encoded string. " .
-                    "This usually implies invalid data was passed to this method. Check that you're passing data correctly.";
-            case self::INVALID_HEADER:
-                return "The header did not match what was expected. " .
-                    "Make sure you're passing the right token to the right methods.";
-            case self::INVALID_JSON:
-                return "When attempting to calculate the recursive depth of the JSON payload, we arrived at " .
-                    "an invalid state. This usually happens when there aren't an equal number of open brackets " .
-                    "and close brackets, or they're not in the proper sequence." .
-                    "This *may* be an attempt to attack the JSON parser library (i.e. stack overflow), " .
-                    "but we detected and prevented this invalid data from reaching the JSON parser.";
-            case self::INVALID_MAC:
-                return "The message authentication code provided with this message did not match the one we calculated.";
-            case self::IMPOSSIBLE_CONDITION:
-                return "A condition we thought impossible has occurred.";
-            case self::INVOKED_RAW_ON_MULTIKEY:
-                return "The raw() method was invoked on a SendingKeyMap or ReceivingKeyMap object, " .
-                    "instead of the actual key. This is an error. Invoke fetch() to get the underling key " .
-                    "and pass that instead.";
-            case self::KEY_NOT_IN_KEYRING:
-                return "The key you requested is not in this keyring.";
-            case self::UNDEFINED_PROPERTY:
-                return "An expected property was not defined at runtime.";
-            case self::INVALID_MESSAGE_LENGTH:
-                return "The received PASETO was too short to be valid.";
-
-            default:
-                return 'Unknown error code';
-        }
+        return match ($code) {
+            self::BAD_VERSION =>
+                "PASETO is very pedantic about versions. " .
+                "If you're getting this message, you were insufficiently pedantic in how you called this feature.",
+            self::CLAIM_JSON_TOO_LONG =>
+                "The length of the JSON-encoded claim sis longer than the Parser is configured to accept.",
+            self::CLAIM_JSON_TOO_MANY_KEYs =>
+                "The number of object keys in the JSON-encoded claims exceeds the configured limit. " .
+                "This may be a Hash-DoS attempt, but it's most likely a misconfiguration.",
+            self::CLAIM_JSON_TOO_DEEP =>
+                "This JSON-encoded claims has too much recursive depth.",
+            self::FOOTER_JSON_ERROR =>
+                "A JSON error occurred when JSON-serializing or deserializing the footer.",
+            self::FOOTER_MISMATCH_EXPECTED =>
+                "We were expecting a different footer than the payload contained.",
+            self::HKDF_BAD_LENGTH =>
+                "At some point, HKDF was called incorrectly. " .
+                "This shouldn't ever happen in practice. Make sure your copy of this library imported correctly.",
+            self::IMPLICIT_ASSERTION_JSON_ERROR =>
+                "A JSON error occurred when JSON-serializing or deserializing the Implicit Assertions.",
+            self::IMPLICIT_ASSERTION_NOT_SUPPORTED =>
+                "Implicit Assertions are only a part of Version 3 and Version 4. " .
+                "This feature is explicitly not permitted in older versions to ensure backwards compatibility.",
+            self::INVALID_BASE64URL =>
+                "An error occurred when attempting to decode what we expected to be a base64url-encoded string. " .
+                "This usually implies invalid data was passed to this method. " .
+                "Check that you're passing data correctly.",
+            self::INVALID_HEADER =>
+                "The header did not match what was expected. " .
+                "Make sure you're passing the right token to the right methods.",
+            self::INVALID_JSON =>
+                "When attempting to calculate the recursive depth of the JSON payload, we arrived at " .
+                "an invalid state. This usually happens when there aren't an equal number of open brackets " .
+                "and close brackets, or they're not in the proper sequence." .
+                "This *may* be an attempt to attack the JSON parser library (i.e. stack overflow), " .
+                "but we detected and prevented this invalid data from reaching the JSON parser.",
+            self::INVALID_MAC =>
+                "The message authentication code provided with this message did not match the one we calculated.",
+            self::IMPOSSIBLE_CONDITION =>
+                "A condition we thought impossible has occurred.",
+            self::INVOKED_RAW_ON_MULTIKEY =>
+                "The raw() method was invoked on a SendingKeyMap or ReceivingKeyMap object, " .
+                "instead of the actual key. This is an error. Invoke fetch() to get the underling key " .
+                "and pass that instead.",
+            self::KEY_NOT_IN_KEYRING =>
+                "The key you requested is not in this keyring.",
+            self::UNDEFINED_PROPERTY =>
+                "An expected property was not defined at runtime.",
+            self::INVALID_MESSAGE_LENGTH =>
+                "The received PASETO was too short to be valid.",
+            default => 'Unknown error code',
+        };
     }
 }
